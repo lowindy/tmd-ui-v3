378122a98f46822c68e78f0d58afd7fda735e6a1
LHC <18979651469@qq.com>
2021-09-22 10:00:32 +0800

    修复axios导入报错

--- src/utils/axios/shims-custom.d.ts
+++ src/utils/axios/shims-custom.d.ts
@@ -1,9 +1,11 @@
+import * as axios from 'axios';
+
 declare module 'axios' {
-  // 定制业务相关的网络请求响应格式， T 是具体的接口返回类型数据
-  export interface CustomSuccessData<T> {
-    code: number;
-    message?: string;
-    data?: T;
-    [keys: string]: any;
-  }
+    // 定制业务相关的网络请求响应格式， T 是具体的接口返回类型数据
+    export interface CustomSuccessData<T> {
+        code: number;
+        message?: string;
+        data?: T;
+        [keys: string]: any;
+    }
 }
